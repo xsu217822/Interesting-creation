32d9d11e5c03e94c805590d6381478141d56f5bd
xsu217822 <143617128+xsu217822@users.noreply.github.com>
2024-01-31 01:16:55 -0500

    demo

---
+++ Demo/Electronic wooden fish/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Electronic_wooden_fish
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            progressBar1 = new ProgressBar();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(130, 406);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(658, 26);
+            progressBar1.TabIndex = 0;
+            progressBar1.Click += progressBar1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft YaHei UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 389);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 52);
+            label1.TabIndex = 1;
+            label1.Text = "功德:";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(812, 450);
+            Controls.Add(label1);
+            Controls.Add(progressBar1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ProgressBar progressBar1;
+        private Label label1;
+    }
+}
---
+++ Demo/Electronic wooden fish/Form1.cs
@@ -0,0 +1,20 @@
+namespace Electronic_wooden_fish
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
