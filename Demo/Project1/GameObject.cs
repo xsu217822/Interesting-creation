03a43722a9a5eb34952de08a51295252dc859441
xsu217822 <143617128+xsu217822@users.noreply.github.com>
2024-02-23 01:59:13 -0500

    update

---
+++ Demo/Project1/GameObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Project1
+{
+    internal class GameObject
+    {
+        public GameObject()
+        {
+
+        }
+    }
+}
